c93bee919c78233e33d4d1bfb9924e6e741c0d89
David Huang <huangd95@gmail.com>
2018-08-22 07:45:13 -0700

    Added .gitignore and README

---
+++ ToneKit/ToneKit.h
@@ -0,0 +1,19 @@
+//
+//  ToneKit.h
+//  ToneKit
+//
+//  Created by DAVID HUANG on 8/22/18.
+//  Copyright © 2018 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ToneKit.
+FOUNDATION_EXPORT double ToneKitVersionNumber;
+
+//! Project version string for ToneKit.
+FOUNDATION_EXPORT const unsigned char ToneKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToneKit/PublicHeader.h>
+
+
